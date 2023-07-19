397bf0f306a8b3852349f743d148b4ebf7034c9d
Chan531 <chan657312@gmail.com>
2023-07-19 19:33:31 +0900

    초기 세팅 완료

---
+++ src/main/java/soongmile/soongmileback/SoongmilebackApplication.java
@@ -0,0 +1,13 @@
+package soongmile.soongmileback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoongmilebackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoongmilebackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/soongmile/soongmileback/SoongmilebackApplicationTests.java
@@ -0,0 +1,13 @@
+package soongmile.soongmileback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoongmilebackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
