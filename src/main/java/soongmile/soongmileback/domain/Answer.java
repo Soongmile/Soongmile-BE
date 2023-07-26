9cb7d104a4a34ee5aeae576d8b773d2c60cdf3dc
Chan531 <chan657312@gmail.com>
2023-07-26 16:22:31 +0900

    feat : Answer entity

---
+++ src/main/java/soongmile/soongmileback/domain/Answer.java
@@ -0,0 +1,32 @@
+package soongmile.soongmileback.domain;
+
+import lombok.Getter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+import static javax.persistence.FetchType.LAZY;
+
+@Entity
+@Getter
+public class Answer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // 본문
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    // 게시 시간
+    @Column(name = "post_time")
+    // @CreatedDate
+    private LocalDateTime postTime;
+
+    @ManyToOne(fetch = LAZY)
+    private Question question;
+
+    @ManyToOne(fetch = LAZY)
+    private User user;
+}
