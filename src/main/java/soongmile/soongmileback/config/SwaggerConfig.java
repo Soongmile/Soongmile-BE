ff1829ebaee735915432281b6fd25da14cab150d
Chan531 <chan657312@gmail.com>
2023-07-27 19:48:17 +0900

    feat : swagger api 초기 세팅

---
+++ src/main/java/soongmile/soongmileback/config/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package soongmile.soongmileback.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import lombok.RequiredArgsConstructor;
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+        info = @Info(title = "숭차로 API 명세서",
+                description = "숭차로 API 명세서",
+                version = "v1"))
+@RequiredArgsConstructor
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi chatOpenApi() {
+        String[] paths = {"/v1/**"};
+
+        return GroupedOpenApi.builder()
+                .group("숭차로 API v1")
+                .pathsToMatch(paths)
+                .build();
+    }
+}
