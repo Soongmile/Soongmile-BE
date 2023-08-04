d004d1137f2da06545376d86e8c940a78a2fbe2f
12betty25 <yoon1004bin@gmail.com>
2023-08-04 16:19:51 +0900

    충돌 수정

--- src/main/java/soongmile/soongmileback/domain/Tag.java
+++ src/main/java/soongmile/soongmileback/domain/Tag.java
@@ -1,36 +1,4 @@
 package soongmile.soongmileback.domain;
 
-
-import lombok.*;
-
-import javax.persistence.*;
-
-import static javax.persistence.EnumType.STRING;
-
-@Entity
-@Getter
-@Setter
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
-@Table(name = "tag")
-public class Tag {
-
-    @Id
-    @GeneratedValue
-    private Long id;
-
-    @Enumerated(STRING)
-    @Column(name = "field", nullable = false)
-    private Field field;
-
-    @Column(name = "tag", nullable = false)
-    private String tagName;
-
-    public static Tag create(Field field, String tagName) {
-        return Tag.builder()
-                .field(field)
-                .tagName(tagName)
-                .build();
-    }
+public enum Tag {
 }
