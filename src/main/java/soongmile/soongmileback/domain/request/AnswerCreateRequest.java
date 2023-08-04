d1ab141c504c255212d042daa70f977dd76d600e
12betty25 <yoon1004bin@gmail.com>
2023-08-04 21:13:36 +0900

    answer

---
+++ src/main/java/soongmile/soongmileback/domain/request/AnswerCreateRequest.java
@@ -0,0 +1,21 @@
+
+package soongmile.soongmileback.domain.request;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+import soongmile.soongmileback.domain.Field;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+@Getter @Setter
+public class AnswerCreateRequest {
+
+    @NotEmpty(message = "본문을 입력해주세요.")
+    @Size(max = 500, message = "제목은 최대 500글자까지 입력할 수 있습니다.")
+    @JsonProperty
+    private String content;
+
+}
+
