fc992a29d75fb63d56cc033d6b0bb7d8fce54b34
Chan531 <chan657312@gmail.com>
2023-08-05 03:57:41 +0900

    git ignore 설정

--- src/main/java/soongmile/soongmileback/service/MemberService.java
+++ src/main/java/soongmile/soongmileback/service/MemberService.java
@@ -1,10 +1,6 @@
 package soongmile.soongmileback.service;
 
 import lombok.RequiredArgsConstructor;
-import lombok.Value;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
