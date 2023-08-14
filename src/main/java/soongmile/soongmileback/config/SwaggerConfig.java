29b298741b4d8c588290e343a561f2928477add3
12betty25 <yoon1004bin@gmail.com>
2023-08-14 19:04:24 +0900

    스웨거 버전 업데이트 및 수정

--- src/main/java/soongmile/soongmileback/config/SwaggerConfig.java
+++ src/main/java/soongmile/soongmileback/config/SwaggerConfig.java
@@ -1,27 +1,58 @@
 package soongmile.soongmileback.config;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
-import lombok.RequiredArgsConstructor;
-import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.*;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
 
-@OpenAPIDefinition(
-        info = @Info(title = "숭차로 API 명세서",
-                description = "숭차로 API 명세서",
-                version = "v1"))
-@RequiredArgsConstructor
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 @Configuration
 public class SwaggerConfig {
 
     @Bean
-    public GroupedOpenApi chatOpenApi() {
-        String[] paths = {"/v1/**"};
-
-        return GroupedOpenApi.builder()
-                .group("숭차로 API v1")
-                .pathsToMatch(paths)
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .securityContexts(Arrays.asList(securityContext()))
+                .securitySchemes(Arrays.asList(apiKey()))
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
                 .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "숭차로 API 명세서",
+                "숭차로 API 명세서",
+                "1.0",
+                "Terms of service",
+                new Contact("Sallo Szrajbman", "www.baeldung.com", "salloszraj@gmail.com"),
+                "License of API",
+                "API license URL",
+                Collections.emptyList());
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder().securityReferences(defaultAuth()).build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(new SecurityReference("JWT", authorizationScopes));
+    }
+
+    // ApiKey 정의
+    private ApiKey apiKey() {
+        return new ApiKey("JWT", "Authorization", "header");
     }
 }
