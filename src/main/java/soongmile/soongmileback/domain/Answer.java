c094ac64f32cf5b38432872f476d8bde75d0db7d
12betty25 <yoon1004bin@gmail.com>
2023-08-16 20:36:12 +0900

    충돌 수정

--- src/main/java/soongmile/soongmileback/domain/Answer.java
+++ src/main/java/soongmile/soongmileback/domain/Answer.java
@@ -1,6 +1,10 @@
 package soongmile.soongmileback.domain;
 
 import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import soongmile.soongmileback.domain.request.AnswerCreateRequest;
