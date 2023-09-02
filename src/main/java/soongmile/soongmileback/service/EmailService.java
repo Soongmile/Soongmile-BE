e4e2c8d7ffdd17aadd9e13184424e1b1c1a3dbbc
Chan531 <chan657312@gmail.com>
2023-09-02 22:16:53 +0900

    feat : 이메일 인증 코드 발송

--- src/main/java/soongmile/soongmileback/service/EmailService.java
+++ src/main/java/soongmile/soongmileback/service/EmailService.java
@@ -1,6 +1,91 @@
 package soongmile.soongmileback.service;
 
-public interface EmailService {
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.MailException;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
 
-    String sendSimpleMessage(String to)throws Exception;
+import javax.mail.Message;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Random;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+
+    private final JavaMailSender emailSender;
+
+    public static final String ePw = createKey();
+
+    private MimeMessage createMessage(String email) throws Exception {
+        System.out.println("보내는 대상 : " + email);
+        System.out.println("인증 번호 : " + ePw);
+        MimeMessage message = emailSender.createMimeMessage();
+
+        message.addRecipients(Message.RecipientType.TO, email); //보내는 대상
+        message.setSubject("이메일 인증 테스트"); // 제목
+
+        String msgg = createMessage();
+
+        message.setText(msgg, "utf-8", "html");//내용
+        message.setFrom(new InternetAddress("soongmile@gmail.com","soongmile")); // 보내는 사람
+
+        return message;
+    }
+
+    private String createMessage() {
+        String msgg="";
+        msgg+= "<div style='margin:20px;'>";
+        msgg+= "<h1> 안녕하세요 숭차로입니다. </h1>";
+        msgg+= "<br>";
+        msgg+= "<p>아래 코드를 복사해 입력해주세요<p>";
+        msgg+= "<br>";
+        msgg+= "<p>감사합니다.<p>";
+        msgg+= "<br>";
+        msgg+= "<div align='center' style='border:1px solid black; font-family:verdana';>";
+        msgg+= "<h3 style='color:blue;'>회원가입 인증 코드입니다.</h3>";
+        msgg+= "<div style='font-size:130%'>";
+        msgg+= "CODE : <strong>";
+        msgg+= ePw+"</strong><div><br/> ";
+        msgg+= "</div>";
+        return msgg;
+    }
+
+    public static String createKey() {
+        StringBuffer key = new StringBuffer();
+        Random rnd = new Random();
+
+        for (int i = 0; i < 8; i++) { // 인증코드 8자리
+            int index = rnd.nextInt(3); // 0~2 까지 랜덤
+
+            switch (index) {
+                case 0:
+                    key.append((char) ((int) (rnd.nextInt(26)) + 97));
+                    //  a~z  (ex. 1+97=98 => (char)98 = 'b')
+                    break;
+                case 1:
+                    key.append((char) ((int) (rnd.nextInt(26)) + 65));
+                    //  A~Z
+                    break;
+                case 2:
+                    key.append((rnd.nextInt(10)));
+                    // 0~9
+                    break;
+            }
+        }
+        return key.toString();
+    }
+
+    public String sendSimpleMessage (String email) throws Exception {
+        MimeMessage message = createMessage(email);
+
+        try {
+            emailSender.send(message);
+        } catch (MailException es){
+            es.printStackTrace();
+            throw new IllegalArgumentException();
+        }
+        return ePw;
+    }
 }
