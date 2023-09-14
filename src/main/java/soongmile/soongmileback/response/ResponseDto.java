c1d9aa2c981bf856cc6467b87cceba4c883a4a52
Chan531 <chan657312@gmail.com>
2023-09-14 10:58:46 +0900

    feat : ResponseDto 추가

---
+++ src/main/java/soongmile/soongmileback/response/ResponseDto.java
@@ -0,0 +1,35 @@
+package soongmile.soongmileback.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public class ResponseDto<T> {
+
+    private final boolean success;
+    private final int code;
+    private final String message;
+    private final T result;
+
+    public static <T> ResponseDto<T> success() {
+        return new ResponseDto<>(true, 200, null, null);
+    }
+
+    public static <T> ResponseDto<T> success(String message) {
+        return new ResponseDto<>(true, 200, message, null);
+    }
+
+    public static <T> ResponseDto<T> success(String message, T result) {
+        return new ResponseDto<>(true, 200, message, result);
+    }
+
+    public static <T> ResponseDto<T> fail(HttpStatus httpStatus, String message) {
+        return new ResponseDto<>(false, httpStatus.value(), message, null);
+    }
+
+    public static <T> ResponseDto<T> fail(HttpStatus httpStatus, String message, T result) {
+        return new ResponseDto<>(false, httpStatus.value(), message, result);
+    }
+}
