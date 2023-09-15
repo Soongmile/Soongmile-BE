90ed1d24a39f704b6882705dea7033238ec649d2
Chan531 <chan657312@gmail.com>
2023-09-15 11:39:22 +0900

    reactor : 메세지 설정

--- src/main/java/soongmile/soongmileback/request/SignInRequest.java
+++ src/main/java/soongmile/soongmileback/request/SignInRequest.java
@@ -7,9 +7,9 @@ import javax.validation.constraints.NotBlank;
 @Data
 public class SignInRequest {
 
-    @NotBlank
+    @NotBlank(message = "이메일을 입력하세요.")
     private String email;
 
-    @NotBlank
+    @NotBlank(message = "비밀번호를 입력하세요.")
     private String password;
 }
