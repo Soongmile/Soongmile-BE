fd6997c92d529ea183e777b6e6e8bd3fa9f52e73
Chan531 <chan657312@gmail.com>
2023-09-15 11:37:41 +0900

    refactor : email과 password에 @NotBlank 설정

--- src/main/java/soongmile/soongmileback/request/SignInRequest.java
+++ src/main/java/soongmile/soongmileback/request/SignInRequest.java
@@ -2,9 +2,14 @@ package soongmile.soongmileback.request;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 public class SignInRequest {
 
+    @NotBlank
     private String email;
+
+    @NotBlank
     private String password;
 }
