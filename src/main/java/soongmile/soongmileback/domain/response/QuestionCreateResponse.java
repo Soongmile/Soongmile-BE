d64051e2f90a976904aa22fdd298f59ed1a1d07e
Chan531 <chan657312@gmail.com>
2023-09-29 21:07:28 +0900

    update : 게시물이 닉네임도 반환하도록 변경

--- src/main/java/soongmile/soongmileback/domain/response/QuestionCreateResponse.java
+++ src/main/java/soongmile/soongmileback/domain/response/QuestionCreateResponse.java
@@ -29,6 +29,9 @@ public class QuestionCreateResponse {
     private Long memberId;
 
     @JsonProperty
+    private String memberName;
+
+    @JsonProperty
     private Integer hits;
 
     @JsonProperty
