9043e075f82888d3f8515436ea51c951983e8d23
12betty25 <yoon1004bin@gmail.com>
2023-11-18 15:05:42 +0900

    cors 오류 수정

--- src/main/java/soongmile/soongmileback/config/CorsConfig.java
+++ src/main/java/soongmile/soongmileback/config/CorsConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import java.util.Arrays;
 
 import java.util.List;
 
@@ -16,8 +17,7 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(List.of("http://localhost:3000"));
-        config.setAllowedOrigins(List.of("https://soongmile-fe.vercel.app/"));
+        config.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://soongmile-fe.vercel.app/"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setExposedHeaders(List.of("*"));
